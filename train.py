d1ad81b9fdb27798a46526b5ffed236352fb5e9e
vmarfus <orionsky@qq.com>
2024-07-23 06:48:01 +0000

    update 1

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("yolov8n.yaml")
-    model.train(data = "my-yolo.yaml", epochs = 1000, imgsz = 640, batch = 200)
+    model = YOLO("yolov8s.yaml")
+    model.train(data = "my-yolo.yaml", epochs = 10000, imgsz = 640, batch = 100)
     
